85927d62d0462774a9ab01ba4f9ee2888145dd34
mishe163 <chencaihua@hujiang.com>
2015-11-03 09:53:27 +0800

    修改页面TITLE, 包含微信端的处理

--- setWeixinTitle.js
+++ setWeixinTitle.js
@@ -1,6 +1,7 @@
 $.extend($, {
     setWeixinTitle: function (title) {
         document.title = title;
+        
         // hack在微信IOS webview中无法修改document.title的情况
         if ($.isWeixin() && $.isIOS()) {
             var $iframe = $('<iframe src="/st/images/icon144.png"></iframe>');
