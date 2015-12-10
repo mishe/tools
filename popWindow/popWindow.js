5ef9215a30d4ed930bca60a3c5ce1847f4d80aa5
mishe <mishe@163.com>
2015-12-10 14:49:13 +0800

    Update popWindow.js

--- popWindow/popWindow.js
+++ popWindow/popWindow.js
@@ -18,25 +18,38 @@ $.extend($,{
                 no:'',
                 uniqID:$.uniqID(),
                 type:1,
+                closeBtn:false,
+                tapMask:false,
                 callback: $.noop()
             },
             opt={},
             win;
         opt= $.extend(defaultOptions,options);
         if(opt.type==1) opt.no=opt.no||'取消';
-        win=$(template(opt));
-        $('body').append(win);
-        win.on('tap','.yes_btn,.no_btn',function(){
-            var pos=false;
-            if($(this).hasClass('yes_btn')){
-                pos=true
-            }
-            if($.type(opt.callback)=='function'){
-                if(opt.callback(pos)!==false){
-                    win.remove();
-                }
-            }else{
+        win=$(popWindow(opt));
+        $('body').append(win).on('tap.popWindow'+opt.uniqID,'.yes_btn,.no_btn,.pop_window_wrap',function(e){
+            var pos=false,
+                obj=$(e.target),
+                obj2=$(e.currentTarget);
+
+            if(obj.hasClass('pop_window_wrap') && opt.tapMask){
                 win.remove();
+                $('body').off('tap.popWindow'+opt.uniqID);
+                return false;
+            }
+            if(obj2.hasClass('yes_btn') || obj2.hasClass('no_btn') ) {
+                if (obj2.hasClass('yes_btn')) {
+                    pos = true
+                }
+                if ($.type(opt.callback) == 'function') {
+                    if (opt.callback(pos) !== false) {
+                        win.remove();
+                        $('body').off('tap.popWindow'+opt.uniqID);
+                    }
+                } else {
+                    win.remove();
+                    $('body').off('tap.popWindow'+opt.uniqID);
+                }
             }
         });
     }
