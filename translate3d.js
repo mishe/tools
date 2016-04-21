824d6dbbea46f737d25138978d085dbe0f1f6a97
mishe <mishe@163.com>
2016-04-21 15:19:51 +0800

    Update translate3d.js

--- translate3d.js
+++ translate3d.js
@@ -1,17 +1,26 @@
 $.extend($,{
-    translate3d: function (obj, x, y, z) {
-        x = x || 0, y = y || 0, z = z || 0;
-        $(obj).css({
-            '-webkit-transform': 'translate3d(' + x + 'px,' + y + 'px,' + z + 'px)',
-            '-moz-transform': 'translate3d(' + x + 'px,' + y + 'px,' + z + 'px)',
-            'transform': 'translate3d(' + x + 'px,' + y + 'px,' + z + 'px)'
+    css3Hack: function css3Hack(key, value) {
+        function ch(key, value) {
+            var d = {}
+            d['-weikit-' + key] = value;
+            d['-moz-' + key] = value;
+            d['-ms-' + key] = value;
+            d[key] = value;
+            return d;
+        }
+
+        $(this).each(function () {
+            var self = $(this)
+            setTimeout(function () {
+                self.css(ch(key, value));
+            }, 0)
         });
     },
-    setTransitionTime: function (obj, num) {
-        $(obj).css({
-            '-webkit-transition': +num + 's',
-            '-moz-transition': +num + 's',
-            'transition': +num + 's'
-        });
-    }
+    translate3d: function (x, y, z) {
+        x = x || 0, y = y || 0, z = z || 0;
+        $(this).css3Hack('transform', 'translate3d(' + x + 'px,' + y + 'px,' + z + 'px)');
+    },
+    setTransitionTime: function (num) {
+        $(this).css3Hack('transition', num + 's');
+    },
 });
